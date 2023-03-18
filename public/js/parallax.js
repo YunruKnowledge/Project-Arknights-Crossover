b150e96138e18e68c46d5cb7ea0b37aff4baf8a0
SkyThe <88000687+YunruKnowledge@users.noreply.github.com>
2023-03-18 20:59:44 +0100

    #4 added parallax offset

--- public/js/parallax.js
+++ public/js/parallax.js
@@ -22,16 +22,26 @@ function parallax(event) {
     // console.log(leftPanel)
     // console.log(rightPanel)
 
-    const offset = 1 // in case we might use a weight/friction later (inside a forEach)
     const leftMidPointX = leftPanelInfo.left + leftPanelInfo.width/2
     const rightMidPointX = rightPanelInfo.left + rightPanelInfo.width/2
 
-    const leftTranslatePosX = ((event.pageX + leftMidPointX - window.innerWidth/2) * offset) / 100;
-    const rightTranslatePosX = ((event.pageX - rightMidPointX) * offset) / 100;
-    const translatePosY = ((event.pageY - window.innerHeight/2) * offset) / 100;
-
+    const leftTranslatePosX = ((event.pageX + leftMidPointX - window.innerWidth/2)) / 100;
+    const rightTranslatePosX = ((event.pageX - rightMidPointX)) / 100;
+    const translatePosY = ((event.pageY - window.innerHeight/2)) / 100;
+    
     leftPanel.style.transform = `translate(${leftTranslatePosX}px, ${translatePosY}px) rotateY(${leftTranslatePosX*0.75}deg) rotateX(${translatePosY*1.5*(-1)}deg)`
     rightPanel.style.transform = `translate(${rightTranslatePosX}px, ${translatePosY}px) rotateY(${rightTranslatePosX*0.75}deg) rotateX(${translatePosY*1.5*(-1)}deg)`
+
+    // Offset parallax for elements inside the panels.
+    const bothPanels = [].concat(Array.prototype.slice.call(leftPanel.children), Array.prototype.slice.call(rightPanel.children));
+    bothPanels.forEach(element=> {
+        if (!element.getAttribute("parallaxOffset")) return
+        console.log(element)
+        const offset = element.getAttribute("parallaxOffset") - 1 
+        console.log(offset);
+        element.style.transform = `translate(${leftTranslatePosX*offset}px, ${translatePosY*offset}px) rotateY(${leftTranslatePosX*0.75*offset}deg) rotateX(${translatePosY*1.5*(-1)*offset}deg)`
+    })
+    
     // console.log(rightMidPointX);
     // console.log(leftMidPointX);
     // console.log(window.innerWidth/2);
