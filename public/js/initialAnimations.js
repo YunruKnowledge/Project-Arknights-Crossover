9424a26b2bc092ed470988537b500731cec49676
SkyThe <88000687+YunruKnowledge@users.noreply.github.com>
2023-03-22 13:09:08 +0100

    removed bad guard clause

--- public/js/initialAnimations.js
+++ public/js/initialAnimations.js
@@ -12,9 +12,10 @@ function AnimationsInit() {
     // set animation for character title/name
     const characterName = document.getElementById("characterName")
     const characterNameTarget = characterName.children[0]
-    if (characterNameTarget.tagName === "IMG") return
-    // console.log(characterNameTarget.previousSibling)
-    characterNameTarget.style.setProperty("--nameAniSet","name_line_ani 2s 1 ease-in-out")
+    if (characterNameTarget.tagName === "SPAN") {
+        // console.log(characterNameTarget.previousSibling)
+        characterNameTarget.style.setProperty("--nameAniSet","name_line_ani 2s 1 ease-in-out")
+    }
 
     
     // Set animation for text (attribute: textWriteAni)
