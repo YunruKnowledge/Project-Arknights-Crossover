3df3af0683e2086820bd973257687dbad80af5fb
SkyThe <88000687+YunruKnowledge@users.noreply.github.com>
2023-03-29 10:21:48 +0200

    local data to server

--- routes/router.js
+++ routes/router.js
@@ -8,40 +8,6 @@ const axios = require('axios');
 const api_key = "XFN6hnyTQegupzD2if96SJWXHfoqRFULMdZpY983dpPd3DyLPANjcoi1fmeHC9GK"
 const api_server = "https://eu-central-1.aws.data.mongodb-api.com/app/data-qysgs/endpoint/data/v1/action/find"
 
-router.get("/character/:query", async (req,res)=>{
-
-    const lookUp = {
-        "name": 1,
-        "description": 1,
-        "speciality": 1,
-        "class": 1,
-        "image": 1,
-        "skill": 1,
-        "star": 1
-    }
-    const query = {
-        "name": {
-            // May disable regex later
-            "$regex": req.params.query,
-            "$options": "i"
-        }
-    }
-    
-    try {
-        // console.log(wano)
-        res.render('index', {
-          fav: await fetchData("other", {"_id": {"$oid": "63ff4b0b973764e06c435c81"}}, {"favicon": 1}, 1),
-          btn: await fetchData("other", {"_id": {"$oid": "6401d35168dff4e1b30e0bfd"}}, {"button": 1}, 1),
-          character: await fetchData("character", query, lookUp, 1),
-          icon: await fetchData("character", null, {"image": 1, "name": 1})
-        });
-    } 
-    catch (error) {
-        console.error(error);
-        res.render('error');
-    }
-});
-
 async function fetchData(collection, query, lookFor, limit = null) {
     
     const config = {
@@ -81,14 +47,51 @@ async function fetchData(collection, query, lookFor, limit = null) {
     return serverData
 };
 
+// routing
+router.get("/character/:query", async (req,res)=>{
+
+    const lookUp = {
+        "name": 1,
+        "description": 1,
+        "speciality": 1,
+        "class": 1,
+        "image": 1,
+        "skill": 1,
+        "star": 1
+    }
+    const query = {
+        "name": {
+            // May disable regex later
+            "$regex": req.params.query,
+            "$options": "i"
+        }
+    }
+
+    try {
+        // console.log(wano)
+        res.render('index', {
+          logo: await fetchData("other", {"_id": {"$oid": "641b037abca27907730374a5"}}, {"logo": 1}, 1),
+          fav: await fetchData("other", {"_id": {"$oid": "63ff4b0b973764e06c435c81"}}, {"favicon": 1}, 1),
+          btn: await fetchData("other", {"_id": {"$oid": "6401d35168dff4e1b30e0bfd"}}, {"button": 1}, 1),
+          tabImg: await fetchData("other", {"_id": {"$oid": "641af63fbca27907730374a3"}}, {"tab": 1}, 1),
+          classImage: await fetchData("other", {"_id": {"$oid": "641af760bca27907730374a4"}}, {"classIcon": 1}, 1),
+          icon: await fetchData("character", null, {"image": 1, "name": 1}),
+          character: await fetchData("character", query, lookUp, 1)
+        });
+    } 
+    catch (error) {
+        console.error(error);
+        res.render("error");
+    }
+});
+
 router.get("*", (req,res)=>{
     try {
-        res.render("error")
+        res.render("404error")
     }
     catch {
         res.render("error")
     }
 });
-
 
 module.exports = router;
